ecff2590b2cc22f82634026286549b7e7e4c18d1
Shintaro Manako <shintaro@Shintaros-MacBook-Air.local>
2024-08-15 22:12:52 +0900

    setup env vars

--- src/common/firebase/firebase.ts
+++ src/common/firebase/firebase.ts
@@ -8,13 +8,13 @@ import { getAuth } from 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	apiKey: 'AIzaSyAhubiRs1sSVE1MeSq8AQ5Ds50LRDaUMB0',
-	authDomain: 'masstodon.firebaseapp.com',
-	projectId: 'masstodon',
-	storageBucket: 'masstodon.appspot.com',
-	messagingSenderId: '380314695830',
-	appId: '1:380314695830:web:eb9c55f10936ad5f8faab7',
-	measurementId: 'G-46MG7C7G9L',
+	apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+	authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+	projectId: import.meta.env.VITE_FIREBASE_PRROJECT_ID,
+	storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+	appId: import.meta.env.VITE_FIREBASE_APP_ID,
+	measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
